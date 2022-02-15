1f2325a5658f41cce0456c17a34ed2b86ee341f5
Gabriel <shalitmontagne.g@gmail.com>
2022-02-14 19:26:54 -0700

    Core Deliverables Complete

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(r => r.json())
+      .then(setPlants)
+  }, [])
+
+  function addPlant(newPlant) {
+    const updatedPlants = [...plants, newPlant]
+    setPlants(updatedPlants)
+  }
+
+  const displayedPlants = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant}/>
+      <Search setSearch={setSearch} />
+      <PlantList plants={displayedPlants} />
     </main>
   );
 }
